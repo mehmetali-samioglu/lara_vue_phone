f55282de2d9695dbf1736267bfb0b366112582e4
sqltimer@gmail.com <Sqlzamani1357>
2018-07-30 12:35:54 +0300

    Ilk Uygulamalar

---
+++ resources/assets/js/components/Myfooter.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "myfooter"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/assets/js/components/Myheader.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "myheader"
+    }
+</script>
+
+<style scoped>
+
+</style>
